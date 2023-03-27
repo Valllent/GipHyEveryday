e8713c59f47aed6b55fadfe89e356f1a16f7fcad
Valentin Petrovych <valllentua9128@gmail.com>
2023-03-27 16:05:15 +0300

    Support paging on the detail screen.

--- app/src/main/java/com/valllent/giphy/GifsViewModel.kt
+++ app/src/main/java/com/valllent/giphy/GifsViewModel.kt
@@ -1,16 +1,11 @@
 package com.valllent.giphy
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
-import com.valllent.giphy.data.Gif
-import com.valllent.giphy.network.GifNetworkDataSource
 import com.valllent.giphy.ui.dataproviders.GifPagingSource
 import com.valllent.giphy.utils.Constants
-import kotlinx.coroutines.delay
 
 class GifsViewModel : BaseViewModel() {
 
@@ -20,34 +15,9 @@ class GifsViewModel : BaseViewModel() {
         FETCHED,
     }
 
-    private val _dataFetchingStatus = mutableStateOf(DataFetchingStatus.IN_PROGRESS)
-    val dataFetchingStatus: State<DataFetchingStatus> = _dataFetchingStatus
-
-    private val _gifs = mutableStateOf<List<Gif>>(emptyList())
-    val gifs: State<List<Gif>> = _gifs
-
     val gifsFlow = Pager(
         config = PagingConfig(pageSize = Constants.ITEMS_COUNT_PER_REQUEST),
         pagingSourceFactory = { GifPagingSource() }
     ).flow.cachedIn(viewModelScope)
-
-    init {
-//        fetchGifs()
-        _dataFetchingStatus.value = DataFetchingStatus.FETCHED
-    }
-
-    fun fetchGifs() {
-        launch {
-            _dataFetchingStatus.value = DataFetchingStatus.IN_PROGRESS
-            delay(1_000)
-            val result = GifNetworkDataSource().getTrending(0)
-            if (result == null) {
-                _dataFetchingStatus.value = DataFetchingStatus.FAILED
-            } else {
-                _gifs.value = result.gifs
-                _dataFetchingStatus.value = DataFetchingStatus.FETCHED
-            }
-        }
-    }
 
 }
--- app/src/main/java/com/valllent/giphy/ui/screens/DetailGifScreen.kt
+++ app/src/main/java/com/valllent/giphy/ui/screens/DetailGifScreen.kt
@@ -1,12 +1,11 @@
 package com.valllent.giphy.ui.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.rememberScrollableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,38 +14,65 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.valllent.giphy.GifsViewModel
 import com.valllent.giphy.R
 import com.valllent.giphy.data.Gif
 import com.valllent.giphy.ui.preview.GifPreviewData
-import com.valllent.giphy.ui.views.ImageFromNetwork
-import com.valllent.giphy.ui.views.SubtitleOnSurface
-import com.valllent.giphy.ui.views.TitleOnSurface
+import com.valllent.giphy.ui.views.*
 import com.valllent.giphy.ui.wrappers.PreviewWrapper
-
-@Composable
-fun DetailGifScreen(gifsViewModel: GifsViewModel, selectedGifIndex: Int) {
-    val gifs = gifsViewModel.gifs.value
-    DetailGifPager(gifs, selectedGifIndex)
-}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-private fun DetailGifPager(gifs: List<Gif>, selectedGifIndex: Int) {
-    val pagerState = rememberPagerState(selectedGifIndex)
+fun DetailGifScreen(gifsViewModel: GifsViewModel, selectedGifIndex: Int) {
+    val lazyGifs = gifsViewModel.gifsFlow.collectAsLazyPagingItems()
+    val appendState = lazyGifs.loadState.append
+
+    val addAdditionalItem = appendState !is LoadState.NotLoading
+    val itemsCount = if (lazyGifs.itemCount > 0) {
+        lazyGifs.itemCount + if (addAdditionalItem) 1 else 0
+    } else {
+        0
+    }
+
     HorizontalPager(
-        gifs.size,
-        state = pagerState
+        itemsCount,
+        state = rememberPagerState(selectedGifIndex),
+        key = { i ->
+            val isPlaceholder = addAdditionalItem && i == lazyGifs.itemCount
+            if (isPlaceholder) {
+                "Placeholder"
+            } else {
+                lazyGifs.peek(i)?.id ?: ""
+            }
+        }
     ) { i ->
-        DetailGif(gifs[i])
+        val isPlaceholder = addAdditionalItem && i == lazyGifs.itemCount
+        if (isPlaceholder) {
+            if (appendState is LoadState.Loading) {
+                InProgress()
+            } else {
+                DataFetchingFailed(
+                    onRetryClick = {
+                        lazyGifs.retry()
+                    }
+                )
+            }
+        } else {
+            lazyGifs[i]?.let {
+                DetailGif(it)
+            }
+        }
     }
 }
 
 @Composable
 private fun DetailGif(gif: Gif) {
     Column(
-        modifier = Modifier.fillMaxSize()
-            .scrollable(rememberScrollableState { 0f }, Orientation.Vertical)
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         TitleOnSurface(
             gif.title,
